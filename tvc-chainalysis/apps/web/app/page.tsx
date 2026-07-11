96b55ff8b844f5ce307405ff291058bd6ffbf7ec
Angel Hernandez <angel@turnkey.io>
2026-07-11 14:34:06 -0400

    fix(web): include SDK callbacks in wallet-bootstrap effect deps

--- tvc-chainalysis/apps/web/app/page.tsx
+++ tvc-chainalysis/apps/web/app/page.tsx
@@ -25,8 +25,7 @@ export default function Home() {
     }
 
     ensureWallet().catch(console.error);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [authState]);
+  }, [authState, createWallet, refreshWallets]);
 
   if (authState !== AuthState.Authenticated) {
     return (
